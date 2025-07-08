37796385d4dc986496efd6ab84f9ab25e6cbf9da
Muhammad Fahreza <yya77591@gmail.com>
2025-07-08 10:30:53 +0700

    updated

---
+++ app/components/lib/projects.ts
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "ReNote",
+    link: "https://github.com/PiggyBank-BangkitCapstoneTeam/ReNote",
+    thumbnail: "/asset/Projects/Renote.png",
+  },
+  {
+    title: "Aplikasi List Hero ML",
+    link: "https://github.com/MhmdFahreza/Aplikasi-Hero-Mobile-Legends",
+    thumbnail: "/asset/Projects/AplikasiListHeroML.png",
+  },
+  {
+    title: "Gear4Play Shop",
+    link: "https://github.com/MhmdFahreza/Gear4play-shop",
+    thumbnail: "/asset/Projects/Gear4playShop.png",
+  },
+  {
+    title: "Pemesanan Tiket Pulau Komodo",
+    link: "https://github.com/MhmdFahreza/JWD_TugasAkhir",
+    thumbnail: "/asset/Projects/WebPariwisataPulauKomodo.png",
+  },
+  {
+    title: "BoxJump",
+    link: "https://github.com/MhmdFahreza/Game-3D-BoxJump",
+    thumbnail: "/asset/Projects/Boxjump.png",
+  },
+  {
+    title: "Pendeteksi Cancer",
+    link: "https://github.com/MhmdFahreza/Pendeteksi-cancer-APK",
+    thumbnail: "/asset/Projects/PendeteksiCancer.png",
+  },
+  {
+    title: "Kopi Kreasi",
+    link: "https://github.com/MhmdFahreza/AdminCafe",
+    thumbnail: "/asset/Projects/KopiKreasi.png",
+  },
+  {
+    title: "Web SKKM",
+    link: "https://github.com/MhmdFahreza/web-skkm",
+    thumbnail: "/asset/Projects/webskkm.png",
+  },
+  {
+    title: "Web Daftar Beasiswa",
+    link: "https://github.com/MhmdFahreza/FormBeasiswa",
+    thumbnail: "/asset/Projects/Formdatabeasiswa.png",
+  },
+  {
+    title: "My Portofolio",
+    link: "https://github.com/MhmdFahreza/my-website-portofolio",
+    thumbnail: "/asset/Projects/MyPortofolio.png",
+  },
+];
